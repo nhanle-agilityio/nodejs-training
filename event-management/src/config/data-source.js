cfa8fc0fe442cbad094db0a5cf65a5c416d72345
Nhan Le <nhan.le@asnet.com.vn>
2026-01-30 18:10:49 +0700

    Create docker file to run app

--- event-management/src/config/data-source.js
+++ event-management/src/config/data-source.js
@@ -7,7 +7,7 @@ import { UserSubscriber } from '../subscribers/userSubscriber.js';
 import { DataSource } from 'typeorm';
 export const AppDataSource = new DataSource({
   type: 'sqlite',
-  database: 'database.sqlite',
+  database: 'database/database.sqlite',
   synchronize: false,
   logging: false,
   entities: [Event, User, RefreshToken],
