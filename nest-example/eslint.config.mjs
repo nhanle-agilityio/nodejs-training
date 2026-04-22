2529ec7a7c42e4df637ee8e5e16f3df453db1e65
Nhan Le <nhanle@MacBook-Pro---Nhan.local>
2026-04-22 14:24:02 +0700

    Update more rules for eslint to check test

--- nest-example/eslint.config.mjs
+++ nest-example/eslint.config.mjs
@@ -3,11 +3,15 @@ import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
+import jest from 'eslint-plugin-jest';
+import jestExtended from 'eslint-plugin-jest-extended';
 
 export default tseslint.config(
   {
     ignores: ['eslint.config.mjs'],
   },
+  jest.configs['flat/recommended'],
+  jestExtended.configs['flat/recommended'],
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   eslintPluginPrettierRecommended,
@@ -30,6 +34,12 @@ export default tseslint.config(
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
       "prettier/prettier": ["error", { endOfLine: "auto" }],
+      "jest/expect-expect": "error", // Ensure that tests have at least one expect statement
+      "jest/no-conditional-expect": "warn", // Prevent conditional expects
+      "jest/prefer-called-with": "error", // Prefer using .toHaveBeenCalledWith
+      "jest/no-disabled-tests": "error", // Disable disabled tests
+      "jest/valid-expect-in-promise": "warn", // Ensure that expect statements are used in promises
+      "jest/no-standalone-expect": "warn", // Prevent standalone expect statements
     },
   },
 );
